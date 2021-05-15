d77aa8fcdfe6fbbcbea0db51f1fbc1647a5a8da4
toothFairyDragon <gabrielle.catania@gmail.com>
2021-05-15 16:32:59 -0700

    Worked on part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,9 +6,45 @@ var tbody = d3.select("tbody");
 
 
 //loop through hw_data.js file 
+/*
 data.forEach(function(UFO_sighting){
     console.log(UFO_sighting);
     var row = tbody.append("tr");
+});
+*/ 
+
+/*data.forEach(function(UFO_sighting){
+    console.log(UFO_sighting);
+    var row = tbody.append("tr");
+
+    Object.entries(UFO_sighting).forEach(function([key, value]){
+        console.log(key, value);
+    });
+
+
+});
+*/
+
+/*data.forEach(function(UFO_sighting){
+    console.log(UFO_sighting);
+    var row = tbody.append("tr");
+
+    Object.entries(UFO_sighting).forEach(function([key, value]){
+        console.log(key, value);
+        var cell = row.append("td");
+    });
+});
+*/
+
+data.forEach(function(UFO_sighting){
+    console.log(UFO_sighting);
+    var row = tbody.append("tr");
+    Object.entries(UFO_sighting).forEach(function([key, value]){
+        console.log(key,value);
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
 });
 
 // YOUR CODE HERE!
