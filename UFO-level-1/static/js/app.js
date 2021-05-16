b603b07fb16e31d7923d76109b049d77bc882819
toothFairyDragon <gabrielle.catania@gmail.com>
2021-05-15 17:11:14 -0700

    Filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -36,7 +36,7 @@ data.forEach(function(UFO_sighting){
 });
 */
 
-data.forEach(function(UFO_sighting){
+/*data.forEach(function(UFO_sighting){
     console.log(UFO_sighting);
     var row = tbody.append("tr");
     Object.entries(UFO_sighting).forEach(function([key, value]){
@@ -46,5 +46,44 @@ data.forEach(function(UFO_sighting){
         cell.text(value);
     });
 });
+*/
+
 
 // YOUR CODE HERE!
+
+//assigns ufo sighting data fromo data.js to variable
+var sightings = data;
+
+//select the button
+var button = d3.select("#filter-btn", pressEnter);
+//select the form 
+var form = d3.select("#datetime", pressEnter);
+
+//create event handlers for both button click and form-enter
+button.on("click", pressEnter);
+form.on("submit", pressEnter);
+
+//event handler function 
+function pressEnter(){
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    console.log("inputValue")
+    console.log(sightings);
+
+    var filteredSightings = sightings.filter(sight => sight.datetime === inputValue);
+    filteredSightings.forEach(function(see){
+        console.log(see);
+        var row = tbody.append("tr");
+        Object.entries(see).forEach(function([key, value]){
+            console.log(key,value);
+
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+    
+
+};
